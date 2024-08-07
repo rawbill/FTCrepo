33e791c9a1c4af1e5f5f951ee4cf424c54a469db
rawbill <siddharth.goli@gmail.com>
2024-08-06 20:06:33 -0400

    Create Mecanum.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/javaClass/Mecanum.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.javaClass;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class Mecanum extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+
+        DcMotor lf_drive = hardwareMap.get(DcMotor.class, "lf_drive");
+        DcMotor rf_drive = hardwareMap.get(DcMotor.class, "rf_drive");
+        DcMotor lb_drive = hardwareMap.get(DcMotor.class, "lb_drive");
+        DcMotor rb_drive = hardwareMap.get(DcMotor.class, "rb_drive");
+
+        lf_drive.setDirection(DcMotorEx.Direction.REVERSE);
+        rf_drive.setDirection(DcMotorEx.Direction.FORWARD);
+        lb_drive.setDirection(DcMotorEx.Direction.REVERSE);
+        rb_drive.setDirection(DcMotorEx.Direction.FORWARD);
+
+        while (opModeIsActive()) {
+
+            double turn   =  gamepad1.right_stick_x;
+            double strafe =  gamepad1.left_stick_x;
+            double drive  = -gamepad1.left_stick_y;
+
+            double lfPower = Range.clip(drive + strafe + turn, -1.0, 1.0);
+            double rfPower = Range.clip(drive - strafe - turn, -1.0, 1.0);
+            double lbPower = Range.clip(drive - strafe + turn, -1.0, 1.0);
+            double rbPower = Range.clip(drive + strafe - turn, -1.0, 1.0);
+
+            lf_drive.setPower(lfPower);
+            rf_drive.setPower(rfPower);
+            lb_drive.setPower(lbPower);
+            rb_drive.setPower(rbPower);
+
+        }
+    }
+}
